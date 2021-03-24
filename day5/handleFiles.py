69a788f92a244ef4928f27afae6a090e91999855
Ivan Fernandez <ivan.ferndz94@gmail.com>
2021-03-24 16:57:13 +0100

    woww

---
+++ day5/handleFiles.py
@@ -0,0 +1,4 @@
+name_handle = open('kids', 'r')
+for i in name_handle:
+    print(i)
+name_handle.close()
---
+++ day5/polygonArea.py
@@ -0,0 +1,8 @@
+import math
+
+
+def polysum(n, s):
+    return round(((0.25*n*s**2) / math.tan(math.pi/n) + (s*n)**2), 4)
+
+
+print(areaPolygon(4, 42))
