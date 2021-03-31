7c4e42893123e703c6febb2159423bfab09c5cdc
Ivan Fernandez <ivan.ferndz94@gmail.com>
2021-04-01 00:44:39 +0200

    Class practice

--- day8/fract.py
+++ day8/fract.py
@@ -6,7 +6,26 @@ class Fraction (object):
     def __str__(self):
         return str(self.num) + ' / ' + str(self.denom)
 
+    def getNumber(self):
+        return self.num
+
+    def getDenom(self):
+        return self.denom
+
+    def __add__(self, other):
+        numberNew = other.getDenom() * self.getNumber() \
+            + other.getNumber() * self.getDenom()
+        denomNew = other.getDenom() * self.getDenom()
+        return Fraction(numberNew, denomNew)
+
+    def __sub__(self, other):
+        numberNew = other.getDenom() * self.getNumber() \
+            - other.getNumber() * self.getDenom()
+        denomNew = other.getDenom() * self.getDenom()
+        return Fraction(numberNew, denomNew)
+
 
 three_eights = Fraction(3, 8)
+one_half = Fraction(1, 2)
 
-print(three_eights)
+print(three_eights.__add__(one_half))
