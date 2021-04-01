d2fa8a968a4e3131841a970d55e74ec4b6dce9d5
Ivan Fernandez <ivan.ferndz94@gmail.com>
2021-04-01 21:56:07 +0200

    More classes

--- day9/moreclasses.py
+++ day9/moreclasses.py
@@ -28,7 +28,54 @@ class Person(object):
         return self.lastName < other.lastName
 
 
-juanito = Person('Juanito García')
+class MITPerson(Person):
+    nextIdNum = 0
 
-juanito.setBirthday(20, 2, 1994)
-print(juanito.getAge())
+    def __init__(self, name):
+        Person.__init__(self, name)
+        self.idNum = MITPerson.nextIdNum
+        MITPerson.nextIdNum += 1
+
+    def getIdNum(self):
+        return self.idNum
+
+    def __lt__(self, other):
+        return self.idNum < other.idNum
+
+    def speak(self, word):
+        return (self.getLastName() + " says : " + word)
+
+
+class UG(MITPerson):
+    def __init__(self, name, classYear):
+        MITPerson.__init__(self, name)
+        self.year = classYear
+
+    def getClass(self):
+        return self.year
+
+    def speak(self, word):
+        return MITPerson.speak(self, "Dude, " + word)
+
+
+class Grad(MITPerson):
+    pass
+
+
+def isStudent(obj):
+    return isinstance(obj, UG) or isinstance(obj, Grad)
+
+
+Louis = MITPerson('Fernando Gomez')
+joseL = MITPerson('Fuck you man')
+
+mattD = UG("Matt Damon", 2020)
+lowerL = UG("Lower Leis", 2019)
+bigD = UG("Big David", 2017)
+
+print(lowerL.speak("Wow this sucks"))
+print(bigD.speak("wooo don't be a David"))
+
+Louis.setBirthday(20, 12, 1930)
+
+print(joseL.speak("Hey mudafucka"))
